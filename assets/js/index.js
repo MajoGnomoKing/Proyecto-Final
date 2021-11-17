b9524ed03ab0828a0e14c94eec8f68a03106dc40
MajoGnomoKing <majotorresacevedo@gmail.com>
2021-11-16 23:07:16 -0500

    Se reemplaza el texto de referencia/relleno por el texto acorde

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const power_button = document.querySelector('.button');
 
 power_button.addEventListener('click', () => {
-document.body.classList.toggle('dark');
-power_button.classList.toggle('button--active');
+    document.body.classList.toggle('dark');
+    power_button.classList.toggle('button--active');
 });
